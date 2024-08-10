7027a9ea5c03369317f8bd20882aedd51fdf46b2
Andreas Bubolz <mail@andreas-bubolz.de>
2024-08-10 23:37:14 +0200

    Abreissen tut

--- src/main/webapp/app/player/textout.component.ts
+++ src/main/webapp/app/player/textout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, AfterViewInit, Input, ElementRef /*inject, OnInit, signal*/ } from '@angular/core';
+import { Component, AfterViewInit, Input, HostListener, ElementRef /*OnDestroy, inject, OnInit, signal*/ } from '@angular/core';
 import {
   GridstackComponent,
   /*GridstackModule,
@@ -20,60 +20,154 @@ import { ContextMenuComponent } from './context-menu.component';
   selector: 'widget-textout',
   standalone: true,
   //template: 'TextOut {{text}}',
-  template: `
-    <div class="grid-stack-item-content" (contextmenu)="onRightClick($event)">{{ text }}</div>
-    <div id="menu-overlay" [style.display]="contextMenuVisible ? 'block' : 'none'" class="draggable">
-      <button id="menu-button" (click)="setFullscreen(false)">Menü</button>
-    </div>
-  `,
+  template: '<div class="grid-stack-item-content">{{ text }}</div>',
 })
-export class TextoutComponent extends BaseWidget implements OnDestroy, AfterViewInit {
+export class TextoutComponent extends BaseWidget implements AfterViewInit {
   @Input() text: string = 'suchmich'; // test custom input data
   public serialize(): NgCompInputs | undefined {
     return this.text ? { text: this.text } : undefined;
   }
-  contextMenuVisible: boolean = false;
-  contextMenuPosition: { x: number; y: number } = { x: 0, y: 0 };
 
   constructor(private elementRef: ElementRef /*private ngZone: NgZone*/) {
     super();
     //GridstackComponent.addComponentToSelectorType([YtPlayerComponent]);
   }
+  private grid: any;
 
   ngAfterViewInit() {
     const nativeElement = this.elementRef.nativeElement as HTMLElement;
     var gridElement = nativeElement.closest('.grid-stack')! as HTMLElement & { gridstack?: any };
-    var grid = gridElement.gridstack;
-    grid.on('dragstart', (event: Event, el: HTMLElement, node: GridStackNode) => this.onDragStart(event, node));
-    grid.on('dragstop', () => this.onDragEnd());
+    this.grid = gridElement.gridstack;
+    //// Events s.u.
+    this.grid.on('dragstart', (event: Event, el: HTMLElement, node: GridStackNode) => this.onDragStart(event, node));
+    this.grid.on('dragstop', () => this.onDragEnd());
   }
 
-  setFullscreen(fullscreen: boolean) {
-    alert('tach');
-  }
-  private timeoutId: any;
+  private startX = 0;
+  private startY = 0;
+
+  private floating = false;
+  private draggableElement: HTMLElement | null = null; // Zuerst macht das das Grid
+  private nonGridDragging = false;
+  private dragInfo = { x: 0, dx: 0, y: 0, dy: 0 };
+
+  private resizing = false;
+  private resizeStartWidth: number = 0;
+  private resizeStartHeight: number = 0;
+  private resizeStartX: number = 0;
+  private resizeStartY: number = 0;
+
+  // Dragging im Grid
+  private timerId: any;
+  private movementThreshold = 20; // Bewegungsschwelle in Pixeln
+  private delay = 3000; // Zeitverzögerung in ms
+  private gridDragging = false;
   onDragStart(event: any, node: any) {
-    this.timeoutId = setTimeout(() => {
-      const nativeElement = this.elementRef.nativeElement as HTMLElement;
-      var gridDomElement = nativeElement.closest('.grid-stack')! as HTMLElement & { gridstack?: any };
-      var grid = gridDomElement.gridstack;
-      var gridItemDomElement = nativeElement.closest('.grid-stack-item')!; // as HTMLElement & { gridstackNode?: any };
-      debugger;
-      grid.removeWidget(gridItemDomElement, false, true);
-      const widget = gridItemDomElement.querySelector('.grid-stack-item-content')! as HTMLElement;
-      widget.style.position = 'absolute';
-      widget.style.left = '0px';
-      widget.classList.add('draggable');
+    this.gridDragging = true;
+    this.startX = event.clientX;
+    this.startY = event.clientY;
+    this.startTimer();
+  }
 
-      //this.changeGridItem(node);
-    }, 2000); // 2000 ms = 2 Sekunden
+  startTimer() {
+    this.timerId = setTimeout(() => {
+      this.makeFloating();
+    }, this.delay);
+  }
+
+  resetTimer() {
+    clearTimeout(this.timerId);
+    this.startTimer();
+  }
+
+  makeFloating() {
+    const nativeElement = this.elementRef.nativeElement as HTMLElement;
+
+    var gridDomElement = nativeElement.closest('.grid-stack')! as HTMLElement & { gridstack?: any };
+    this.grid = gridDomElement.gridstack;
+
+    var gridItemDomElement = nativeElement.closest('.grid-stack-item')!;
+    this.grid.removeWidget(gridItemDomElement, false, true);
+
+    this.draggableElement = gridItemDomElement.querySelector('.grid-stack-item-content')! as HTMLElement;
+    let de = this.draggableElement;
+    de.style.position = 'absolute';
+    de.classList.add('draggable');
+    de.addEventListener('mousedown', e => this.onMouseDown(e));
+    de.addEventListener('mousemove', (e: MouseEvent) => this.onMouseMove(e));
+    de.addEventListener('mouseup', e => this.onMouseUp(e));
+
+    this.floating = true;
+    alert('floating');
   }
 
   onDragEnd() {
-    clearTimeout(this.timeoutId);
+    clearTimeout(this.timerId);
   }
 
-  ngOnDestroy() {
-    console.log('Textout destroyed.');
-  } // test to make sure cleanup happens
+  @HostListener('document:mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    if (!this.floating || event.target != this.draggableElement) return;
+
+    event.stopPropagation(); // Stop the event from reaching the grid
+    event.preventDefault();
+    this.grid.enableMove(false);
+    var target = this.draggableElement!;
+    if (target.classList.contains('resizable') && event.offsetX > target.offsetWidth - 10 && event.offsetY > target.offsetHeight - 10) {
+      this.resizing = true;
+      this.resizeStartWidth = target.offsetWidth;
+      this.resizeStartHeight = target.offsetHeight;
+      this.resizeStartX = event.clientX;
+      this.resizeStartY = event.clientY;
+    } else {
+      this.dragInfo.x = event.clientX - this.dragInfo.dx;
+      this.dragInfo.y = event.clientY - this.dragInfo.dy;
+      this.nonGridDragging = true;
+    }
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (!this.floating && this.gridDragging) {
+      const deltaX = Math.abs(event.clientX - this.startX);
+      const deltaY = Math.abs(event.clientY - this.startY);
+
+      console.log('!!!1222234');
+      if (deltaX > this.movementThreshold || deltaY > this.movementThreshold) {
+        // Reset the timer if the movement exceeds the threshold
+        this.resetTimer();
+        this.startX = event.clientX;
+        this.startY = event.clientY;
+      }
+    }
+    if (!this.floating || (event.target != this.draggableElement && !this.nonGridDragging))
+      // sonst reissts ab, wen man schnell draggt
+      return;
+
+    if (this.floating) {
+      if (this.resizing) {
+        event.preventDefault();
+        var target = this.draggableElement!;
+        const newWidth = this.resizeStartWidth + (event.clientX - this.resizeStartX);
+        const newHeight = this.resizeStartHeight + (event.clientY - this.resizeStartY);
+        target.style.width = `${newWidth}px`;
+        target.style.height = `${newHeight}px`;
+      } else if (this.nonGridDragging) {
+        event.preventDefault();
+        this.dragInfo.dx = event.clientX - this.dragInfo.x;
+        this.dragInfo.dy = event.clientY - this.dragInfo.y;
+        this.draggableElement!.style.transform = `translate3d(${this.dragInfo.dx}px, ${this.dragInfo.dy}px, 0)`;
+      }
+    }
+  }
+
+  @HostListener('document:mouseup', ['$event'])
+  onMouseUp(event: MouseEvent) {
+    if (event.target != this.draggableElement) return;
+
+    this.gridDragging = false;
+    this.nonGridDragging = false;
+    this.resizing = false;
+    this.grid.enableMove(true);
+  }
 }
